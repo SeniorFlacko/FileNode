bf52b540e8ce8f5f920ed438ce431b6397a94c9b
Cristian <calemanfuentes@amarello.com.mx>
2018-12-20 15:24:47 -0600

    Separacion de la configuracion de yargs en un archivo separado

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 // Required Externo
 // const fs = require('express');
 
-// Required de archivos locales
+// Required de archivos locales mediante destructuracion del objeto exports del global module
 const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
-const argv = require('yargs')
-                .command('listar','Imprime en consola la tabla de multiplicar',{
-                    base: {
-                        demand: true,
-                        alias: 'b'
-                    },
-                    limite:{
-                        alias: 'l',
-                        default: 10
-                    }
-                })
-                .command('crear','Crea un archivo con la tabla de multiplicar de dicha base',{
-                    base: {
-                        demand: true,
-                        alias: 'b'
-                    },
-                    limite: {
-                        alias: 'l',
-                        default: 10
-                    },
-                })
-                .help()
-                .argv;
+const { argv }  = require('./config/yargs');
+
 // Base Manual:
 // let base = 9;
 
@@ -42,8 +21,6 @@ let comando = argv._[0]; // listar -l=11 -b=3 | crear -b=7
 switch (comando) {
     case 'listar':
         listarTabla(argv.base, argv.limite);
-            // .then( archivo => console.log(`Archivo creado: ${ archivo }`))
-            // .catch( err => console.log( err ));
         break;
     case 'crear':
         crearArchivo(argv.base,argv.limite)
